efdd1b31508fab1af8860c09c3010e1c899c6dae
chengchingwen <adgjl5645@hotmail.com>
2018-03-31 15:55:48 +0800

    update eatools with upstream(scitail)
           (diff.py out of date)

--- eatools/ea.py
+++ eatools/ea.py
@@ -1,11 +1,43 @@
 import json as jn
 from numpy import argmax
 
-ifile = "SciTailV1/dgem_format/input.jsonl"
-ofile = "SciTailV1/dgem_format/output.jsonl"
+models = ['dgem', 'decompatt', 'overlap']
+while True:
+    model = input(f"analyze model ({', '.join(models)}[or index]): ").strip()
+    if model in models:
+        break
+    else:
+        try:
+            idx = int(model)
+            model = models[idx]
+            break
+        except ValueError:
+            print(f"{model} model not exist.")
+
+
+predict_file = f"predictions/scitail_1.0_{model}_predictions_dev.jsonl"
 eafile = input("filename(default = result.jsonl): ")
 eafile = eafile if eafile != "" else "result.jsonl"
 
+def get_legal_key(filename):
+    with open(filename, "r") as f:
+        l = f.readline()
+        d = jn.loads(l)
+        return list(d.keys())
+
+key_descri = {'gold_label(label)' : 'Final entailment label for the premise=KB Sentence and hypothesis=Q+A as Sentence from {entails, neutral}',
+              'predict' : 'final prediction',
+              'sentence1(premise)' : 'the premise',
+              'sentence2(hypothesis)' : 'the hypothesis',
+              'sentence2_structure(hypothesis_structure)':'structure from the hypothesis in the same format as described in dgem_format/README.txt',
+              'question' : 'Original question',
+              'answer' : 'Answer to the original question',
+              'score' : 'entailment probs',
+              'label_probs' : 'probs of labels {neutral, entails}',
+              'label_logits' : 'logits of labels {neutral, entails}',
+              }
+legal_keys = get_legal_key(predict_file)
+added_keys = ["premise", "hypothesis", "label", "predict", "hypothesis_structure"]
 flag = False
 pre_cmd = ''
 count = 1
@@ -77,6 +109,9 @@ def prompt(js):
         elif r == '' and (pre_cmd == 'n' or pre_cmd == 'nw'):
             r = pre_cmd
             continue
+        elif r == 'kd':
+            for k in key_descri:
+                print(f"{k}: {key_descri[k]}")
         elif r == 'hps':
             print(js.get("hypothesis_structure"))
         elif r == 'd':
@@ -95,45 +130,46 @@ def prompt(js):
                   "j + #: jump to number",
                   "e: add error description",
                   "w: write sample",
+                  "kd: print key description",
                   "hps: print hypothesis_structure",
                   "d: dump json",
-                  "s + key: print value (opt: ['premise', 'hypothesis', 'label', 'hypothesis_structure', 'label_logits', 'label_probs', 'loss', 'predict'])\n", sep="\n")
+                  f"s + key: print value (opt: ({', '.join(legal_keys+added_keys)}))\n", sep="\n")
         pre_cmd = r
         r = input("?> ").strip()
 
 
-with open(ifile, "r") as fi, open(ofile, "r") as fo, open(eafile, "w+") as ff:
+with open(predict_file, "r") as fp, open(eafile, "w+") as ff:
     label = ['neutral', 'entails']
     while count:
-        fi.seek(0)
-        fo.seek(0)
-        for i, (il, ol) in enumerate(zip(fi, fo)):
+        fp.seek(0)
+        for i, lp in enumerate(fp):
             if jump > i:
                 continue
             elif jump < i:
                 break
             else:
                 jump += 1
-            ji,jo = jn.loads(il),jn.loads(ol)
-            ji.update(jo)
+            jp = jn.loads(lp)
+            jp["premise"] = jp["sentence1"]
+            jp["hypothesis"] = jp["sentence2"]
+            jp["hypothesis_structure"] = jp["sentence2_structure"]
+            jp["label"] = jp["gold_label"]
 
-
-            pred = label[argmax(ji["label_probs"])]
-            ans = pred == ji["label"]
-            ji["predict"] = pred
+            pred = label[argmax(jp["label_probs"])]
+            ans = pred == jp["label"]
+            jp["predict"] = pred
 
             if flag == True and ans == True:
                 continue
 
-            js = jn.dumps(ji)
+            js = jn.dumps(jp)
             print(i,":", end="")
-            print_sample(ji)
-            # print(js)
-            # print("predict: ", pred)
+            print_sample(jp)
             print("correct" if ans else "wrong")
 
-            keep = prompt(ji)
+            keep = prompt(jp)
             if not keep:
                 exit("[Exist]")
         
 
+#"""
